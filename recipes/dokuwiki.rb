6c0f46fe0c7cffba5d04c966d5ee06887d7bb718
Miguel Armas <kuko@canarytek.com>
2014-06-29 17:22:28 +0100

    Added totals to reports

--- recipes/dokuwiki.rb
+++ recipes/dokuwiki.rb
@@ -17,6 +17,28 @@
 # limitations under the License.
 #
 
+# Get totals for alerts
+def update_totals(totals,n,key,subkey)
+  if n.has_key?('rasca_status')
+    if n['rasca_status'].has_key?(key) && n['rasca_status'][key].has_key?(subkey) and n['rasca_status'][key][subkey].has_key?("last_status")
+      case n['rasca_status'][key][subkey]["last_status"]
+        when "OK"
+          totals[key]["OK"]+=1
+        when "WARNING"
+          totals[key]["WARNING"]+=1
+        when "CRITICAL"
+          totals[key]["CRITICAL"]+=1
+        when "UNKNOWN"
+          totals[key]["UNKNOWN"]+=1
+        when "RECOVERED"
+          totals[key]["RECOVERED"]+=1
+      end
+    end
+  end
+  totals
+end
+
+
 # Get admindomains
 admindomains=search(:role, "admindomain:*").sort!{|x, y| x.name <=> y.name}
 
@@ -46,19 +68,60 @@ admindomains.each do |admindomain|
       variables(:n => n)
     end
   end
- 
+  ## Reports
+  directory "#{node['modularit-inventory']['dokuwiki']['data_dir']}/#{admindomain.name}/rasca_reports" do
+    owner node['modularit-inventory']['dokuwiki']['owner']
+    group node['modularit-inventory']['dokuwiki']['group']
+  end
+  # Backups
+  template "#{node['modularit-inventory']['dokuwiki']['data_dir']}/#{admindomain.name}/rasca_reports/backup.txt" do
+    source 'report_backup.dokuwiki.erb'
+    owner node['modularit-inventory']['dokuwiki']['owner']
+    group node['modularit-inventory']['dokuwiki']['group']
+    mode 00640
+    variables(:nodes => search(:node, "admindomain:#{admindomain.name}").sort!{|x, y| x.name <=> y.name})
+  end
+  # Security
+  template "#{node['modularit-inventory']['dokuwiki']['data_dir']}/#{admindomain.name}/rasca_reports/security.txt" do
+    source 'report_security.dokuwiki.erb'
+    owner node['modularit-inventory']['dokuwiki']['owner']
+    group node['modularit-inventory']['dokuwiki']['group']
+    mode 00640
+    variables(:nodes => search(:node, "admindomain:#{admindomain.name}").sort!{|x, y| x.name <=> y.name})
+  end
 end
 
-## Reports
+## Global Reports
 directory "#{node['modularit-inventory']['dokuwiki']['data_dir']}/rasca_reports" do
   owner node['modularit-inventory']['dokuwiki']['owner']
   group node['modularit-inventory']['dokuwiki']['group']
 end
 # Backups
+totals={ 
+  "CheckDuplicity" => { "OK" => 0, "WARNING" => 0, "CRITICAL" => 0, "UNKNOWN" => 0, "RECOVERED" => 0 },
+  "CheckBackup" => { "OK" => 0, "WARNING" => 0, "CRITICAL" => 0, "UNKNOWN" => 0, "RECOVERED" => 0 },
+}
+mynodes = search(:node, "nagios_name:*").sort!{|x, y| x.name <=> y.name}
+mynodes.each do |n|
+  totals=update_totals(totals,n,"CheckDuplicity","CheckDuplicity")
+  totals=update_totals(totals,n,"CheckBackup","CheckBackup")
+end
 template "#{node['modularit-inventory']['dokuwiki']['data_dir']}/rasca_reports/backup.txt" do
-  source 'report_backup.dokuwiki.erb'
+  source 'global_report_backup.dokuwiki.erb'
   owner node['modularit-inventory']['dokuwiki']['owner']
   group node['modularit-inventory']['dokuwiki']['group']
   mode 00640
-  variables(:nodes => search(:node, "*:*").sort!{|x, y| x.name <=> y.name})
+  variables(
+    :admindomains => search(:role, "admindomain:*").sort!{|x, y| x.name <=> y.name},
+    :totals => totals
+  )
 end
+# Security
+template "#{node['modularit-inventory']['dokuwiki']['data_dir']}/rasca_reports/security.txt" do
+  source 'global_report_security.dokuwiki.erb'
+  owner node['modularit-inventory']['dokuwiki']['owner']
+  group node['modularit-inventory']['dokuwiki']['group']
+  mode 00640
+  variables(:admindomains => search(:role, "admindomain:*").sort!{|x, y| x.name <=> y.name})
+end
+
