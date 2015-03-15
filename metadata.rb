f680c85ee40f7cdaa58769b4c078c4defb5458b8
Miguel Armas <kuko@canarytek.com>
2015-03-15 23:30:11 +0000

    fix links to guest in node report

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ license           "Apache 2.0"
 description       "Creates an inventory based on Chef data"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "0.0.1"
+name              "modularit-inventory"
 recipe            "dokuwiki", "Create the inventory in dokuwiki format"
 
 #%w{apt yum}.each do |pkg|
